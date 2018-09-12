0cda14faf917c11e1433dfe13c7584a447bb802f
AbbeyGB <abbey.bolina@gmail.com>
2018-09-12 11:57:54 -0700

    Added link to Progression check in step2 HTML page

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,10 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
+#@app.route('/index')
+@app.route('/step2')
+#@app.route('step3')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    return render_template('step2.html')
+    #return render_template('step3.html')
