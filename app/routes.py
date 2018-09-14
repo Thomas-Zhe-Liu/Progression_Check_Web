d8a678fe2d928ca4451bc74f9f6ad591fe184bda
AbbeyGB <abbey.bolina@gmail.com>
2018-09-13 21:11:03 -0700

    Added Table for the Courses and Formatted the Container/Side Menu

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,11 @@ from flask import render_template
 
 @app.route('/')
 #@app.route('/index')
-@app.route('/step2')
+@app.route('/step1')
+#@app.route('/step2')
 #@app.route('step3')
 def index():
     #return render_template('index.html')
-    return render_template('step2.html')
+    return render_template('step1.html')
+    #return render_template('step2.html')
     #return render_template('step3.html')
