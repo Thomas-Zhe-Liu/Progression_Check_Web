00c780ce7fc90f0614c4096514d80c9a3f22f3e3
z5059479 <z5059479@student.unsw.edu.au>
2018-09-18 22:04:39 +1000

    index.htm routed with routes.py, user inout prgram_code, commence_year and major code to go to next stage(choose finished course

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
 from app import app
-from flask import render_template
+from flask import Flask, redirect, render_template, request, url_for
 
-@app.route('/')
-#@app.route('/index')
-#@app.route('/step1')
-@app.route('/step2')
-#@app.route('step3')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    #return render_template('index.html')
-    #return render_template('step1.html')
-    return render_template('step2.html')
-    #return render_template('step3.html')
+	if request.method == "POST":
+		program_code = request.form["program_code"]
+		#check if it is a integer, beeter be drop list
+		commence_year = int(request.form["commence_year"])
+		major = request.form["major"]
+		#check if all the form is selected
+		print("hello " + str(commence_year))
+		return redirect(url_for("step2", program_code=program_code, commence_year=commence_year, major=major))
+	return render_template('index.html')
+
+@app.route("/step2/<program_code>/<commence_year>/<major>", methods=["GET", "POST"])
+def step2(program_code, commence_year, major):
+	return render_template('step2.html', program_code = program_code, commence_year = commence_year, major = major)
