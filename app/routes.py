7e2f6d918c1a4105ac85133adae0df6724d852d5
z5059479 <z5059479@student.unsw.edu.au>
2018-09-20 09:59:26 +1000

    routes from step 1 to step 2

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import Flask, redirect, render_template, request, url_for
 from core_course import *
 from program import *
+from major import *
 
 #@app.route("/", methods=["GET", "POST"])
 @app.route('/')
@@ -18,18 +19,29 @@ def index():
 # 		return redirect(url_for("step2", program_code=program_code, commence_year=commence_year, major=major))
 # 	return render_template('index.html')
 
-@app.route('/step1')
+@app.route('/step1', methods=["GET", "POST"])
 def step1():
+	#user input program_code, commence_year and major code, get these data and redirect to step2.html
+	if request.method == "POST":
+		#request.form["program"] return 3778 Computer Science, we need to get 3778
+		program_code = request.form["program"].split()[0]
+		commence_year = int(request.form["c_year"])
+		#request.form["major"] return COMPA1 COMPUTER SCIENCE, we need to get COMPA1
+		major = request.form["major"].split()[0]
+		return redirect(url_for("step2", program_code = program_code, commence_year = commence_year, major = major))
 	all_programs = get_all_programs_code_name()
-	return render_template('step1.html',all_programs = all_programs)
+	#hard coded needs to replace later
+	all_majors = get_majors_of_a_program(3778, 2019)
+	return render_template('step1.html', all_programs = all_programs, all_majors = all_majors)
 
-@app.route('/step2')
-def step2():
-   return render_template('step2.html')
+@app.route('/step2/<program_code>/<commence_year>/<major>', methods=["GET", "POST"])
+def step2(program_code, commence_year, major):
+	print(program_code + " " + str(commence_year) + " " + major)
+	return render_template('step2.html', program_code = program_code, commence_year = commence_year, major = major)
 
 @app.route('/step3')
 def step3():
-    return render_template('step3.html')
+	return render_template('step3.html')
 
 # @app.route("/step2/<program_code>/<commence_year>/<major>", methods=["GET", "POST"])
 # def step2(program_code, commence_year, major):
