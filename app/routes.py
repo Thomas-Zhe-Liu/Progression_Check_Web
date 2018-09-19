d9101e20648ee441ebb6c159e466836a27d45784
AbbeyGB <abbey.bolina@gmail.com>
2018-09-19 12:15:47 -0700

    Added some changes temporarily to redirect to step 1 page

--- app/routes.py
+++ app/routes.py
@@ -3,15 +3,20 @@ from flask import Flask, redirect, render_template, request, url_for
 import core_course
 
 @app.route("/", methods=["GET", "POST"])
+
+@app.route('/index')
 def index():
-	if request.method == "POST":
-		program_code = request.form["program_code"]
-		#check if it is a integer, beeter be drop list
-		commence_year = int(request.form["commence_year"])
-		major = request.form["major"]
-		#check if all the form is selected
-		return redirect(url_for("step2", program_code=program_code, commence_year=commence_year, major=major))
-	return render_template('index.html')
+    return render_template('index.html')
+
+# def index():
+# 	if request.method == "POST":
+# 		program_code = request.form["program_code"]
+# 		#check if it is a integer, beeter be drop list
+# 		commence_year = int(request.form["commence_year"])
+# 		major = request.form["major"]
+# 		#check if all the form is selected
+# 		return redirect(url_for("step2", program_code=program_code, commence_year=commence_year, major=major))
+# 	return render_template('index.html')
 
 @app.route('/step1')
 def step1():
