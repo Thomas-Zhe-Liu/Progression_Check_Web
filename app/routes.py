8ccbe081b74facfd8f99162054fdfa1a4db28d5d
AbbeyGB <abbey.bolina@gmail.com>
2018-09-19 12:12:34 -0700

    Made changes to Routes.py to re-route to different page

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,18 @@ def index():
 		return redirect(url_for("step2", program_code=program_code, commence_year=commence_year, major=major))
 	return render_template('index.html')
 
+@app.route('/step1')
+def step1():
+    return render_template('step1.html')
+
+#@app.route('/step2')
+#def step2():
+#    return render_template('step2.html')
+
+@app.route('/step3')
+def step3():
+    return render_template('step3.html')
+
 @app.route("/step2/<program_code>/<commence_year>/<major>", methods=["GET", "POST"])
 def step2(program_code, commence_year, major):
 	return render_template('step2.html', program_code = program_code, commence_year = commence_year, major = major)
