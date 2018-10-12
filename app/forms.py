9db683ecaa13324b00abdb7f972d327998c9d8a9
toneeey <z5113238@zmail.unsw.edu.au>
2018-10-12 21:46:48 +1100

    wtform for registering with basic validation
    only checks if fields arent null, email is an email and password and confirm password match

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
