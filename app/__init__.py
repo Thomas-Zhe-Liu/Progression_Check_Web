86a4a60e4bd0237ff24307df72e725cad0226ccc
Toneeey <toneey@live.com>
2018-10-17 13:24:59 +1100

    model class for Users and login_manager setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
 
 from app import routes
